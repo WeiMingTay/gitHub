2a930260a0d3975238613bb28af17fbc92c06e36
dennfa <dennis_fank@web.de>
2023-09-08 11:37:13 +0200

    Added funnyNames

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,13 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<String> funnyNames = new ArrayList<>();
+        funnyNames.add("Doodelidoo");
+        funnyNames.add("Poochilala");
+        System.out.println(funnyNames);
     }
 }
